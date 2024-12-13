8b65ef49cf3f9543ad75585eea3ab1188fcc476a
redjax <no@none.com>
2024-12-13 01:57:17 -0500

    Move all dependencies to global uv project.
    Remove years/2024 workspace

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code
+
+My solutions for [Advent of Code](https://adventofcode.com). Most solutions are in Python, and may include a Jupyter notebook.
+
+## Years
+
+- [2024](./years/2024/)
--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import logging
 import socket
+from pathlib import Path
 
 import nox
 
@@ -9,10 +10,47 @@ log = logging.getLogger("nox")
 logging.basicConfig(level="DEBUG", format="%(asctime)s %(levelname)s %(name)s:%(lineno)d %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
 
+# this VENV_DIR constant specifies the name of the dir that the `dev`
+# session will create, containing the virtualenv;
+# the `resolve()` makes it portable
+VENV_DIR = Path("./.venv").resolve()
+
+
+def install_uv_project(session: nox.Session, external: bool = False) -> None:
+    """Method to install uv and the current project in a nox session."""
+    log.info("Installing uv in session")
+    session.install("uv")
+    log.info("Syncing uv project")
+    session.run("uv", "sync", external=external)
+    log.info("Installing project")
+    session.run("uv", "pip", "install", ".", external=external)
+
+
+def find_free_port(start_port=8000) -> int:
+    """Find a free port starting from a specific port number."""
+    port = start_port
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            try:
+                sock.bind(("0.0.0.0", port))
+                return port
+            except socket.error:
+                log.info(f"Port {port} is in use, trying the next port.")
+                port += 1
+                
+
 @nox.session(name="ruff-lint")
 def ruff_lint(session: nox.Session):
     log.info("Installing ruff")
     session.install("ruff")
     
     log.info("Checking code with ruff")
-    session.run("ruff", "check", "--fix", "years", "noxfile.py")
+    session.run("jupyter", "notebook", "--no-browser", "--ip=0.0.0.0", "--port=8000", "--NotebookApp.token=''", "--NotebookApp.password=''")
+
+
+@nox.session(name="start-jupyter")
+def start_jupyter(session: nox.Session):
+    install_uv_project(session=session, external=False)
+    port = find_free_port()
+    log.info(f"Starting Jupyter on port {port}")
+    session.run("jupyter", "notebook", "--port", str(port))
