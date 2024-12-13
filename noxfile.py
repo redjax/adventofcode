9383a5018b6cce4bed68546064a77042cf366c3e
redjax <no@none.com>
2024-12-13 01:10:37 -0500

    Update noxfile.
    Update day 1 part 1 notebook, add saving results to file

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,17 @@
 from __future__ import annotations
+import logging
 
+import socket
 import nox
+
+log = logging.getLogger("nox")
+logging.basicConfig(level="DEBUG", format="%(asctime)s %(levelname)s %(name)s:%(lineno)d %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+
 
 @nox.session(name="ruff-lint")
 def ruff_lint(session: nox.Session):
+    log.info("Installing ruff")
     session.install("ruff")
+    
+    log.info("Checking code with ruff")
     session.run("ruff", "check", "--fix", "years", "noxfile.py")
